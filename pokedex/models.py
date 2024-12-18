bc0d09ac50de360e73afd5857fcb4d2823e8cb0a
Patricia Sotalin <apsotalin@puce.edu.ec>
2024-12-18 17:44:57 -0500

    Laboratorio 8

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+
+      pass
+      name=models.CharField(max_length=30, null=False) #Para vachar 
+      type=models.CharField(max_length=30, null=False)
+      weight=models.CharField(max_length=6 ,decimal_places=4)
+      weight=models.CharField(max_length=6 ,decimal_places=4)
+      
+      def __str__(self):
+            return self.name
+        
